2535286a54d20aba51c166e0ac169941445dc6d5
necastle88 <devLifeNC@gmail.com>
2021-02-05 17:48:52 -0800

    Added scroll features that include on scroll show btn and onscroll add active a class

--- js/app.js
+++ js/app.js
@@ -1,55 +1,128 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+/* Global Variables */
+const sections = document.getElementsByTagName("section");
+const navList = document.getElementById("navbar__list");
+const getScrollToTopBtn = document.getElementById("scroll-to-top-btn");
 
-/**
- * Define Global Variables
- * 
-*/
+/* Functions */
 
+// Adds menu navbar__menu class to nav
+const addMenuClass = (navList) => {
+  return navList.classList.add("navbar__menu");
+};
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+// Dynamic Nav
+const buildNav = (sections, navList) => {
+  for (const section of sections) {
+    const linkList = document.createElement("li");
+    const linkItem = document.createElement("a");
+    const sectionData = section.getAttribute("data-nav");
+    const sectionId = section.getAttribute("id");
 
+    linkItem.setAttribute("href", `#${sectionId}`);
+    linkItem.classList.add("menu__link");
+    linkItem.classList.add(`${sectionId}`);
+    linkItem.textContent = `${sectionData}`;
 
+    navList.appendChild(linkList).appendChild(linkItem);
+  }
+};
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+// Hides the nav with the person is scrolling down
+const hideNavWhenScrolling = (navList) => {
+  let previousScrollPosition = window.pageYOffset;
+  let isScrolling;
 
-// build the nav
+  window.addEventListener(
+    "scroll",
+    () => {
+      let currentScrollPostion = Math.floor(window.pageYOffset);
 
+      window.clearTimeout(isScrolling);
+      isScrolling = setTimeout(() => {
+        navList.classList.add("navbar__menu");
+        navList.classList.remove("navbar__menu-hidden");
+      }, 400);
 
-// Add class 'active' to section when near top of viewport
+      if (
+        previousScrollPosition < currentScrollPostion &&
+        currentScrollPostion > 300
+      ) {
+        navList.classList.remove("navbar__menu");
+        navList.classList.add("navbar__menu-hidden");
+      } else {
+        navList.classList.add("navbar__menu");
+        navList.classList.remove("navbar__menu-hidden");
+      }
+      previousScrollPosition = currentScrollPostion;
+    },
+    false
+  );
+};
 
+// Shows the to top button after the person has scrolled past 300 Y offset 
+const showToTopButton = (getScrollToTopBtn) => {
+  let previousScrollPosition = window.pageYOffset;
 
-// Scroll to anchor ID using scrollTO event
+  window.addEventListener("scroll", () => {
+      let currentScrollPostion = Math.ceil(window.pageYOffset);
 
+      if (
+        previousScrollPosition + 300 < currentScrollPostion ||
+        currentScrollPostion >= 300
+      ) {
+        getScrollToTopBtn.classList.remove("landing__button--hide");
+        getScrollToTopBtn.classList.add("landing__button--to-top");
+      } else {
+        getScrollToTopBtn.classList.remove("landing__button--to-top");
+        getScrollToTopBtn.classList.add("landing__button--hide");
+      }
+      previousScrollPosition = currentScrollPostion;
+    },
+    false
+  );
+};
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+// Sets current section to active on scroll and highlight current nav section
+const highlightNavLinkOnScroll = () => {
+  window.addEventListener("scroll", () => {
+    let currentSection = "";
 
-// Build menu 
+    for (let section of sections) {
+      const sectionId = section.getAttribute("id");
+      currentSection = document.querySelector(`.${sectionId}`);
+      const sectionTop = section.offsetTop;
+      if (pageYOffset + 150 > sectionTop && pageYOffset - 150 < sectionTop) {
+        currentSection.classList.add("active");
+      } else {
+        currentSection.classList.remove("active");
+      }
+    }
+  });
+};
 
-// Scroll to section on link click
+const toTopOnClick = (btn) => {
+  btn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+    });
+  });
+};
 
-// Set sections as active
+// Adds class to nav so styles stayt consitent
+addMenuClass(navList);
+
+// Build menu
+buildNav(sections, navList);
+
+// Hilight nav onscroll
+highlightNavLinkOnScroll();
+
+// Sides nav when scrolling down
+hideNavWhenScrolling(navList);
+
+// Shows the to top button after the person has scrolled past 300 Y offset 
+showToTopButton(getScrollToTopBtn);
+
+// Scrolls to top on click
+toTopOnClick(getScrollToTopBtn);
